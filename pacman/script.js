ba97775963d6bd425b0675a486daa16bb586a1aa
TheKiruls <amirin856@gmail.com>
2024-06-02 16:24:53 +0800

    done pacman

--- pacman/script.js
+++ pacman/script.js
@@ -10,65 +10,91 @@ const leftKey = document.querySelector("#left");
 const rightKey = document.querySelector("#right");
 const downKey = document.querySelector("#down");
 
+// get the width and height of the container and pacman
+// The offsetWidth  and offsetHeight property returns the viewable width of an element (in pixels) including padding, border and scrollbar, but not the margin.
+// The offsetWidth and offsetHeight property is read-only.
+const containerWidth = document.querySelector("#gameField").offsetWidth;
+const containerHeight = document.querySelector("#gameField").offsetHeight;
+const pacmanWidth = pacman.offsetWidth;
+const pacmanHeight = pacman.offsetHeight;
+
+function moveRight() {
+  // assing pacman styling
+  pacman.style.transform = "rotate(0deg) scaleX(1)";
+  // default value || (parseInt change to integer)
+  const currentRight = parseInt(pacman.style.left || 0);
+  const newRight = currentRight + 50; 
+  // ensure pacman does not move outside the right boundary
+  if (newRight + pacmanWidth <= containerWidth) {
+    pacman.style.left = newRight + "px";
+  } else {
+    pacman.style.left = (containerWidth - pacmanWidth) + "px";
+  };
+  console.log("Right", currentRight, newRight, pacmanWidth, containerWidth);
+}; 
+
+function moveLeft() {
+  // assing pacman styling
+  pacman.style.transform = "rotate(0deg) scaleX(-1)";
+  // default value || (parseInt change to integer)
+  const currentLeft = parseInt(pacman.style.left || 0);
+  const newLeft = currentLeft - 50;
+  containerWidth.offsetWidth;
+  if (newLeft >= 0) {
+    pacman.style.left = newLeft + "px";
+  } else {
+    pacman.style.left = 0 + "px";
+  };
+  console.log ("Left", currentLeft, newLeft);
+};
+
+function moveUp() {
+  // assing pacman styling
+  pacman.style.transform = "rotate(-90deg) scaleX(1)";
+  // default value || (parseInt change to integer)
+  const currentTop = parseInt(pacman.style.top || 0);
+  const newTop = currentTop - 50;
+  if (newTop >= 0) {
+    pacman.style.top = newTop + "px";
+  } else {
+  pacman.style.top = 0 + "px";
+  };
+  console.log ("Up", currentTop, newTop);
+};
+
+function moveDown() {
+  // assing pacman styling
+  pacman.style.transform = "rotate(90deg) scaleX(1)";
+  // default value || (parseInt change to integer)
+  const currentDown = parseInt(pacman.style.top || 0);
+  const newDown = currentDown + 50;
+  // ensure the pacman does not move outside the bottom boundary
+  if (newDown + pacmanHeight <= containerHeight) {
+    pacman.style.top = newDown + "px";
+  } else {
+    pacman.style.top = (containerHeight - pacmanHeight) + "px";
+  };
+  console.log("Down", currentDown, newDown, pacmanHeight, containerHeight);
+}; 
+
 // detect keypress
 document.addEventListener("keydown", function (event) {
   if (event.key === "ArrowRight") {
-    console.log("Right");
-    //   assing pacman styling
-    pacman.style.transform = "rotate(0deg) scaleX(1)";
-    // default value || (parseInt change to integer)
-    const currentLeft = parseInt(pacman.style.left || 0);
-    const newLeft = currentLeft + 50;
-    pacman.style.left = newLeft + "px";
-    console.log(currentLeft, newLeft);
+    moveRight();
   } else if (event.key === "ArrowLeft") {
-    console.log("Left");
-    //   assing pacman styling
-    pacman.style.transform = "rotate(0deg) scaleX(-1)";
-    const currentLeft = parseInt(pacman.style.left || 0);
-    const newLeft = currentLeft - 50;
-    if (newLeft >= 0) {
-      pacman.style.left = newLeft + "px";
-    } else {
-      pacman.style.left = 0 + "px";
-    }
-    pacman.style.left = newLeft + "px";
-    console.log(currentLeft, newLeft);
+    moveLeft();
   } else if (event.key === "ArrowUp") {
-    console.log("Up");
-    //   assing pacman styling
-    pacman.style.transform = "rotate(-90deg) scaleX(1)";
-    const currentTop = parseInt(pacman.style.top || 0);
-    const newTop = currentTop - 50;
-    pacman.style.top = newTop + "px";
-    console.log(currentTop, newTop);
+    moveUp();
   } else if (event.key === "ArrowDown") {
-    console.log("Down");
-    //   assing pacman styling
-    pacman.style.transform = "rotate(90deg) scaleX(1)";
-    const currentTop = parseInt(pacman.style.top || 0);
-    const newTop = currentTop + 50;
-    pacman.style.top = newTop + "px";
-    console.log(currentTop, newTop);
+    moveDown();
   }
-
 });
 
-rightKey.addEventListener("click", function () {
-  let = pacman.style.transform = "rotate(0deg) scaleX(1)";
-});
-
-leftKey.addEventListener("click", function () {
-  let = pacman.style.transform = "rotate(0deg) scaleX(-1)";
-});
-
-upKey.addEventListener("click", function () {
-  let = pacman.style.transform = "rotate(-90deg) scaleX(1)";
-});
-
-downKey.addEventListener("click", function () {
-  let = pacman.style.transform = "rotate(90deg) scaleX(1)";
-});
+// assign action to the button
+rightKey.addEventListener("click", moveRight);
+leftKey.addEventListener("click", moveLeft);
+upKey.addEventListener("click", moveUp);
+downKey.addEventListener("click", moveDown);
 
 
 // assignment for pacman
